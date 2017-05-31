b1a1a711ee9764683d289d98c563681c5382c449
Nicolas Relange <nrelange@lemoinetechnologies.com>
2017-05-31 08:49:33 +0200

    More logs

--- tasks/jsofocamlbuild.js
+++ tasks/jsofocamlbuild.js
@@ -22,36 +22,44 @@ module.exports = function(grunt) {
       var ocamlbuildDone = function (error, result, code) {
         grunt.verbose.writeln('ocamlbuild done');
         if (error) {
+          grunt.log.writeln (result);
           grunt.fail.warn ('ocamlbuild error ' + error);
         }
         else {
-          grunt.verbose.writeln('ocamlbuild successful: ');
+          grunt.verbose.ok('ocamlbuild successful');
+          grunt.verbose.writeln(result);
           jsOfOCaml (jsPath, byteFile);
         }
       }
 
       grunt.verbose.writeln('Byte file is  ' + byteFile);
       grunt.log.writeln('Compiling ' + jsFile + ' into ' + destination);
+      var ocamlbuild_args = options.ocamlbuild.concat (['-use-ocamlfind', byteFile]);
+      grunt.log.writeln('ocamlbuild ' + ocamlbuild_args.join(' '));
       grunt.util.spawn({
         cmd: 'ocamlbuild',
-        args: options.ocamlbuild.concat (['-use-ocamlfind', byteFile])
+        args: ocamlbuild_args
       }, ocamlbuildDone);
     }
 
     var jsOfOCaml = function (jsPath, byteFile) {
       var jsOfOCamlDone = function (error, result, code) {
         if (error) {
+          grunt.log.writeln(result);
           grunt.fail.warn ('js_of_ocaml error ' + error);
         }
         else {
           grunt.log.ok (jsPath + ' was successfully created');
+          grunt.verbose.writeln(result);
           done ();
         }
       }
 
+      var js_of_ocaml_args = options.js_of_ocaml.concat (['-o', jsPath, byteFile]);
+      grunt.log.writeln('js_of_ocaml ' + js_of_ocaml_args.join(' '));
       grunt.util.spawn({
         cmd: 'js_of_ocaml',
-        args: options.js_of_ocaml.concat (['-o', jsPath, byteFile])
+        args: js_of_ocaml_args
       }, jsOfOCamlDone);
     }
 
